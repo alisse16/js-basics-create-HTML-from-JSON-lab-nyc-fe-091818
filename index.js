05e8a74b09ff35f32ae640469cf5a19261c4ef85
Alisse Frandsen <alisse.frandsen@gmail.com>
2018-11-01 23:25:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
-document.addEventListener("DOMContentloaded", (e) => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
+  const titanicButton = document.querySelector('#Titanic')
+  const terminatorButton = document.querySelector('#Terminator-2')
+  
+  titanicButton.addEventListener('click', fillInMovieData)
 });
+
+function fillInMovieData(){
+  const currentMovie = movies['Titanic']
+  
+  const title = currentMovie.title //find the thing
+  const titleDiv = document.querySelector('#title') //find the spot
+  titleDiv.innerText = title //put it there 
+  
+  const director = currentMovie.director
+  const directorDiv = document.querySelector('#director') 
+  directorDiv.innerText = director 
+  
+  const genre = currentMovie.genre
+  const genreDiv = document.querySelector('#genre') 
+  genreDiv.innerText = genre 
+  
+  const filmRating = currentMovie.filmRating
+  const filmRatingDiv = document.querySelector('#filmRating') 
+  filmRatingDiv.innerText = filmRating 
+  
+  const poster = currentMovie.poster
+  const posterImg = document.querySelector('#poster')
+  posterImg.src = poster 
+  
+  const description = currentMovie.description
+  const descriptionDiv = document.querySelector('#description') 
+  descriptionDiv.innerText = description
+  
+  const audienceScore = currentMovie.audienceScore
+  const audienceScoreDiv = document.querySelector('#audienceScore') 
+  audienceScoreDiv.innerText = audienceScore
+  
+  //cast and reviews are arrays
+  for (let castMember in currentMovie.cast) {
+   const li = document.createElement('li') //make the element 
+   li.innerText = castMember.role + ':' + castMember.actor //fill the element
+   const div = document.querySelector('#cast')// put it on the page
+   div.appendChild(li) //put it on the page 
+}
+
